facd8fa6fc22bb144c60937f1c84e8e88124e05e
kmader <kevinmader@4quant.com>
2016-10-18 10:10:40 +0200

    updated bolt code and pyqae libraries

--- pyqae/nd/__init__.py
+++ pyqae/nd/__init__.py
@@ -1,2 +1,14 @@
 # Tools for ND data processing 
 from bolt import *
+from bolt.spark.construct import ConstructSpark as cs
+sp_array = cs.array
+import numpy as np
+
+def meshgridnd_like(in_img, rng_func = range):
+    """
+    Makes a n-d meshgrid in the shape of the input image
+    """
+    new_shape = list(in_img.shape)
+    fixed_shape = [new_shape[1], new_shape[0]]+new_shape[2:] if len(new_shape)>=2 else new_shape 
+    all_range = [rng_func(i_len) for i_len in fixed_shape]
+    return np.meshgrid(*all_range)
