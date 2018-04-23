6d31a202471a50a2ce59a415de98634ace6124c1
Kevin Mader <kevinmader@gmail.com>
2018-04-23 17:17:18 +0200

    adding dask notebooks, updating circle and travis code

--- circleci/execute.sh
+++ circleci/execute.sh
@@ -6,7 +6,7 @@ python --version
 python -c "import numpy; print('numpy %s' % numpy.__version__)"
 python -c "import scipy; print('scipy %s' % scipy.__version__)"
 
-# Generating lectures
+# Generating notebooks
 for nb in notebooks/*ipynb; do
     jupyter nbconvert --ExecutePreprocessor.timeout=3600 --execute "$nb" --to markdown |& tee nb_to_md.txt
     traceback=$(grep "Traceback (most recent call last):" nb_to_md.txt)
@@ -17,8 +17,18 @@ done
 
 . ${PYQAE_HOME}/run-tests.sh
 
+# Generating pipeline notebooks
+for nb in notebooks/Pipelines/*ipynb; do
+    jupyter nbconvert --ExecutePreprocessor.timeout=3600 --execute "$nb" --to markdown |& tee nb_to_md.txt
+    traceback=$(grep "Traceback (most recent call last):" nb_to_md.txt)
+    if [[ $traceback ]]; then
+        exit 1
+    fi
+done
+
 cd ~
 mkdir -p ${PYQAE_HOME}/doc
 mkdir -p ${PYQAE_HOME}/doc/notebooks
-cp -r ${PYQAE_HOME}/notebooks/* ${PYQAE_HOME}/doc/notebooks
+mkdir -p ${PYQAE_HOME}/doc/Pipelines
+cp -r ${PYQAE_HOME}/notebooks/Pipelines/* ${PYQAE_HOME}/doc/Pipelines
 cp -r ./doc ${CIRCLE_ARTIFACTS}
